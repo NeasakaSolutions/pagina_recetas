2c15c4df51cf1b6b92c75a7f61d1666b47e0ffb3
Ubaldo <satou.matsuzaka.3443@gmail.com>
2025-07-10 12:51:03 -0600

    Se agrego la ruta de la app categorias

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,10 +2,16 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf import settings
+from django.conf import static
 
 urlpatterns = [
     # Es la pagina principal de Django y no puede ser modificada
     path('admin/', admin.site.urls),
     # Ruta principal creada por nosotros
     path('', include('home.urls')),
+    path('api/v1/', include('categorias.urls')),
 ]
+
+# Url para configuracion de los archivos que se van a mostrar
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
