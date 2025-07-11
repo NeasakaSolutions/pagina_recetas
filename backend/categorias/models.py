8dec0470d4e3bec078f9d06c5efa2e2954e9ee1c
Ubaldo <satou.matsuzaka.3443@gmail.com>
2025-07-10 22:38:30 -0600

    Se agregaron los modelos para la creacion de la tabla de categorias en la bd

--- backend/categorias/models.py
+++ backend/categorias/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from autoslug import AutoSlugField
 
-# Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length = 100, null = False)
+    slug = AutoSlugField(populate_from = 'nombre')
+
+    # Buenas practicas de programacion: (OPCIONAL)
+    def __str__(self):
+        return self.nombre # Muestra el contenido del campo que se establezca
+    
+    # Nombre que recibira la tabla en el administrador de django
+    class Meta:
+        db_table = 'categorias'
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+
