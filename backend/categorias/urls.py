d6f2e6215c05e272d48dc01d5486c756ffaa4301
Ubaldo <satou.matsuzaka.3443@gmail.com>
2025-07-11 12:25:30 -0600

    Se agrego la ruta de consulta individiual

--- backend/categorias/urls.py
+++ backend/categorias/urls.py
@@ -1,9 +1,11 @@
 # Importaciones
 from django.urls import path
-from .views import Clase1
+from categorias.views import Clase1
+from categorias.views import Clase2
 
 urlpatterns = [
-    path('categorias', Clase1.as_view())
+    path('categorias', Clase1.as_view()),
+    path('categorias/<int:id>', Clase2.as_view()),
 ]
 
 
