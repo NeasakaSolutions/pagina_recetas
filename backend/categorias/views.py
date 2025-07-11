88b3b4cd408d9432f0b289bbe348cf0d476e0edc
Ubaldo <satou.matsuzaka.3443@gmail.com>
2025-07-11 11:32:34 -0600

    Se agrego el metodo get

--- backend/categorias/views.py
+++ backend/categorias/views.py
@@ -1,9 +1,21 @@
+# Importaciones
 from rest_framework.views import APIView
 from django.http.response import JsonResponse
+from categorias.serializers import CategoriaSerializer
+from http import HTTPStatus
+# Tabla que se usara para realizar las consultas
+from categorias.models import Categoria 
 
 # Clases de la aplicacion
 class Clase1(APIView):
 
     # Funcion para mostrar datos
     def get(self, request):
-        pass
+ 
+        # SELECT * FROM categorias ORDER BY DESC;
+        data = Categoria.objects.order_by('-id').all() # Variable que almacena la tabla a consultar
+        datos_json = CategoriaSerializer(data, many = True)
+        return JsonResponse({"data": datos_json.data}, status = HTTPStatus.OK)
+
+
+        
