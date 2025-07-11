776503746bee0cf4a8c65d2775d369a61abe7a0c
Ubaldo <satou.matsuzaka.3443@gmail.com>
2025-07-10 22:38:59 -0600

    Se agrego una clase de testeo

--- backend/categorias/views.py
+++ backend/categorias/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from django.http.response import JsonResponse
 
-# Create your views here.
+# Clases de la aplicacion
+class Clase1(APIView):
+
+    # Funcion para mostrar datos
+    def get(self, request):
+        pass
