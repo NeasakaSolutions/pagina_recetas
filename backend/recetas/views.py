bbd17864e4dfec5c229ef255701719cbf4b51334
Ubaldo <satou.matsuzaka.3443@gmail.com>
2025-07-13 22:50:41 -0600

    Se agrego la funcion de get

--- backend/recetas/views.py
+++ backend/recetas/views.py
@@ -4,9 +4,15 @@ from django.http.response import JsonResponse
 from http import HTTPStatus
 from django.http import Http404
 from django.utils.text import slugify
+from recetas.serializers import RecetaSerializer
+from recetas.models import Receta
 
-# Clases
+# Clase para busquedas grupales
 class Clase1(APIView):
 
     def get (self, request):
-        pass
+
+        # Listar todos los id ordenados de mayor a menor
+        data = Receta.objects.order_by('-id').all()
+        datos_json = RecetaSerializer(data, many = True)
+        return JsonResponse({"data": datos_json.data})
