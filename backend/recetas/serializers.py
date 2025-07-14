dbef1f03d349ea240ea42cb0ae183ab3f5354411
Ubaldo <satou.matsuzaka.3443@gmail.com>
2025-07-14 13:09:14 -0600

    Se corrigieron algunas cosas y agregaron urls

--- backend/recetas/serializers.py
+++ backend/recetas/serializers.py
@@ -1,6 +1,8 @@
 # Importaciones
 from rest_framework import serializers
 from recetas.models import Receta
+from dotenv import load_dotenv
+import os
 
 class RecetaSerializer(serializers.ModelSerializer):
     
@@ -8,9 +10,20 @@ class RecetaSerializer(serializers.ModelSerializer):
     categoria = serializers.ReadOnlyField(source = "categoria.nombre")
     #categoria = serializers.CharField(source = "categoria.nombre")
     fecha = serializers.DateTimeField(format = "%d/%m/%Y") # 13/10/2025
+    # Creamos el campo para la funcion
+    imagen = serializers.SerializerMethodField()
 
     class Meta:
         # Modelo que se usara
         model = Receta
         # Datos que consultaran
-        fields = ("id", "nombre", "slug", "tiempo", "descripcion", "fecha", "categoria", "categoria_id")
+        fields = ("id", "nombre", "slug", "tiempo", "descripcion", "fecha", "categoria", "categoria_id", 
+                "imagen")
+
+    # Funcion para el formateo de la imagen
+    def get_imagen(self, obj):
+
+        #return f"Hola Mundo {obj.id}"
+        #return(os.getenv("settings.BASE_URL"))
+        return f"{os.getenv("BASE_URL")}uploads/recetas/{obj.foto}"
+
