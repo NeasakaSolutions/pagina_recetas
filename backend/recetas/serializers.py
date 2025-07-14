683db054e64d352f987afbfa1d7981b803010ee0
Ubaldo <satou.matsuzaka.3443@gmail.com>
2025-07-13 23:16:38 -0600

    Se agregaron los serializaers.py

--- backend/recetas/serializers.py
+++ backend/recetas/serializers.py
@@ -4,8 +4,13 @@ from recetas.models import Receta
 
 class RecetaSerializer(serializers.ModelSerializer):
     
+    # Muestra el valor del campo que se encuentra en la tabla y no solo el llave foranea
+    categoria = serializers.ReadOnlyField(source = "categoria.nombre")
+    #categoria = serializers.CharField(source = "categoria.nombre")
+    fecha = serializers.DateTimeField(format = "%d/%m/%Y") # 13/10/2025
+
     class Meta:
         # Modelo que se usara
         model = Receta
         # Datos que consultaran
-        fields = ('__all__')
+        fields = ("id", "nombre", "slug", "tiempo", "descripcion", "fecha", "categoria", "categoria_id")
