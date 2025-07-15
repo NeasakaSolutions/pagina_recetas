22bd05750b7d58e74e87994648460c1a759ed7a0
Ubaldo <satou.matsuzaka.3443@gmail.com>
2025-07-15 11:40:26 -0600

    Se agrego la url para la clase2 de la app recetas

--- backend/recetas/urls.py
+++ backend/recetas/urls.py
@@ -1,7 +1,9 @@
 # Importaciones
 from django.urls import path
 from recetas.views import Clase1
+from recetas.views import Clase2
 
 urlpatterns = [
     path('recetas', Clase1.as_view()),
+    path('recetas/<int:id>', Clase2.as_view())
 ]
