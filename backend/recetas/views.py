82bd981942da388ca47516a7f68b58e6c74b956a
Ubaldo <satou.matsuzaka.3443@gmail.com>
2025-07-15 11:40:46 -0600

    Se agrego la clase2

--- backend/recetas/views.py
+++ backend/recetas/views.py
@@ -6,6 +6,9 @@ from django.http import Http404
 from django.utils.text import slugify
 from recetas.serializers import RecetaSerializer
 from recetas.models import Receta
+from django.utils.dateformat import DateFormat
+from dotenv import load_dotenv
+import os
 
 # Clase para busquedas grupales
 class Clase1(APIView):
@@ -16,3 +19,18 @@ class Clase1(APIView):
         data = Receta.objects.order_by('-id').all()
         datos_json = RecetaSerializer(data, many = True)
         return JsonResponse({"data": datos_json.data})
+    
+# Clase para busqueda individual:
+class Clase2(APIView):
+
+    def get(self, request, id):
+        try:
+            data = Receta.objects.filter(id = id).get()
+            return JsonResponse({"data": {"id" : data.id, "nombre": data.nombre, "slug": data.slug, 
+                                "tiempo": data.tiempo, "descripcion": data.descripcion, 
+                                "fecha": DateFormat(data.fecha).format('d/m/Y'), "categoria_id": data.categoria_id, 
+                                "categoria" : data.categoria.nombre, 
+                                "imagen": f"{os.getenv("BASE_URL")}uploads/recetas/{data.foto}"}}, 
+                                status = HTTPStatus.OK)
+        except Receta.DoesNotExist:
+            raise Http404
