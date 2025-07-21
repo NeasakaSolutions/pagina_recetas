c839524bb770750dc4f7335749e43442c6b9deae
Ubaldo <satou.matsuzaka.3443@gmail.com>
2025-07-21 10:30:49 -0600

    Se agregaron modelos

--- backend/seguridad/models.py
+++ backend/seguridad/models.py
@@ -1,3 +1,18 @@
+# Importaciones
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserMetadata(models.Model):
+    user = models.ForeignKey(User, models.DO_NOTHING)
+    # Crear un token para verificaciones de la cuenta
+    token = models.CharField(max_length = 100, blank = True, null = True)
+
+    def __str__(self):
+        # Muestra el primer nombre y el apellido
+        return f"{self.first_user} {self.last_name}"
+    
+    class Meta:
+        db_table = 'users_metadata'
+        verbose_name = "User metadata"
+        verbose_name_plural = "User metadata"
